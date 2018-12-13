553ba5e78aaffe7c6602d5fec4757d9323c36ff0
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2018-12-13 13:17:40 -0600

    Se realiza carpeta que contendrá los ejercicios de Java, el cuál contiene el ejercicio de IF-ELSE

---
+++ Exercises_Java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        SchoolGrades grades= new SchoolGrades(32);
+        grades.knowingMyGrade();
+        System.out.println(grades.getAge());
+
+    }
+}
