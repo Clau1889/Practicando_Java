2c6ee7469f2b57c1ceb45cf71f8af83b1fe8f884
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2018-12-14 10:44:22 -0600

    Se agrega nueva clase que recorre a tráves de un array la lista de animales disponibles en la tienda de mascotas.

--- Exercises_Java/src/Main.java
+++ Exercises_Java/src/Main.java
@@ -1,12 +1,25 @@
 public class Main {
     public static void main(String[] args){
+        /*Put any age and discover your school Grade
+        * IF-ELSE
+        */
         SchoolGrades grades= new SchoolGrades(32);
         grades.knowingMyGrade();
         System.out.println(grades.getAge());
 
-
+        System.out.println();
+        /* Choices: Action, Fiction, Comedy, drama, fantasy, musical, love, thriller, scary and take movie recomendations
+        * SWITCH
+        */
         MovieTheater clasifyCinema = new MovieTheater("comedy");
         clasifyCinema.chooseCategory(clasifyCinema.getCategory());
 
+        System.out.println();
+        /* Finding current avaible pets in PetShop
+        * ARRAY
+        */
+        PetShop pet= new PetShop();
+        System.out.println("Current avaible pets: " + pet.pets.length);
+        pet.avaiblePets();
     }
 }
---
+++ Exercises_Java/src/PetShop.java
@@ -0,0 +1,14 @@
+/* Example ARRAY  */
+
+public class PetShop {
+
+    String[] pets = {"Dog", "Cat", "Snake", "Tarantula", "Birds", "Hamster", "Rabbit"};
+
+    public void avaiblePets(){
+        for (String animals : pets){
+            System.out.println(animals);
+        }
+    }
+
+
+}
